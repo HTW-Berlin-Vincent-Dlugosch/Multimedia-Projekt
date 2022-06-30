f9a6531ddc0c071569cfdde473bdc5449eed7a2b
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-06-30 21:21:06 +0200

    light is now dependant on room light

--- Filter_Zoo/Assets/Scripts/PortTest.cs
+++ Filter_Zoo/Assets/Scripts/PortTest.cs
@@ -1,33 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System.IO.Ports;
+using UnityEngine;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
 
 public class PortTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    SerialPort serialPort;
-
-    void Start()
+  // Start is called before the first frame update
+  SerialPort serialPort;
+  Thread readSerialPortThread;
+  public Light mainLight;
+  double brightness = 0;
+  // public Light mainLight;
+  void Start()
   {
-        serialPort = new SerialPort("COM3", 9600);
-        serialPort.Open();
-        serialPort.ReadTimeout = 1;
-    }
+    serialPort = new SerialPort(portName: "COM3", baudRate: 9600);
+    serialPort.Open();
+    serialPort.ReadTimeout = 50;
+    readSerialPortThread = new Thread(ReadSerialPort);
+    readSerialPortThread.Start();
+  }
 
+  void ReadSerialPort()
+  {
+    Queue<int> queue = new Queue<int>();
+
+    while (true)
+    {
+      try
+      {
+        string line = serialPort.ReadLine();
+        var measuredBrightness = int.Parse(line);
+        queue.Enqueue(measuredBrightness);
+        if (queue.Count > 50)
+        {
+          queue.Dequeue();
+        }
+        brightness = queue.Average() / 1000f;
+      }
+      catch (System.TimeoutException)
+      {
+      }
+    }
+  }
   // Update is called once per frame
   void Update()
   {
-        if (serialPort.IsOpen)
-        {
-            try
-            {
-                Debug.Log(serialPort.ReadLine());
-            }
-            catch (System.Exception)
-            {
+    mainLight.intensity = (float)brightness;
 
-            }
-        }
+  }
+
+  void OnDestroy()
+  {
+    readSerialPortThread.Abort();
+    serialPort.Close();
   }
 }

07d573d3568e49aedf868f5d3132bcd8cd53961c
Max Morgel <Max.Morgel@Student.HTW-Berlin.de>
2022-06-30 17:27:15 +0200

    added more spawner and waypoints

--- Filter_Zoo/Assets/Scripts/CreatureMovement.cs
+++ Filter_Zoo/Assets/Scripts/CreatureMovement.cs
@@ -14,9 +14,43 @@ public class CreatureMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var pos = this.transform.position;
 
-        waypointsBefore = GameObject.FindGameObjectsWithTag("Waypoints");
-        waypoints = new Transform[waypointsBefore.Length];
+        if (pos.x > -16 && pos.x < 46 &&
+       pos.z > 24 && pos.z < 46)
+        {
+            waypointsBefore = GameObject.FindGameObjectsWithTag("Waypoints");
+            waypoints = new Transform[waypointsBefore.Length];
+        }
+
+        if (pos.x > -46 && pos.x < -26 &&
+       pos.z > -28 && pos.z < 44)
+        {
+            waypointsBefore = GameObject.FindGameObjectsWithTag("Waypoints2");
+            waypoints = new Transform[waypointsBefore.Length];
+        }
+
+        if (pos.x > -45 && pos.x < 85 &&
+       pos.z > -46 && pos.z < -37)
+        {
+            waypointsBefore = GameObject.FindGameObjectsWithTag("Waypoints3");
+            waypoints = new Transform[waypointsBefore.Length];
+        }
+
+        if (pos.x > -15 && pos.x < 45 &&
+       pos.z > -26.5 && pos.z < 15.5)
+        {
+            waypointsBefore = GameObject.FindGameObjectsWithTag("Waypoints4");
+            waypoints = new Transform[waypointsBefore.Length];
+        }
+
+        if (pos.x > 54 && pos.x < 84 &&
+       pos.z > -26 && pos.z < 46)
+        {
+            waypointsBefore = GameObject.FindGameObjectsWithTag("Waypoints5");
+            waypoints = new Transform[waypointsBefore.Length];
+        }
+
         Transform switchWaypoint;
         for (int i = 0; i < waypointsBefore.Length; i++)
         {

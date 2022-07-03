dab1241d67c76c9a82e0d578e5d9647d89229697
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-07-03 16:19:10 +0200

    add ambient light with arduino

--- Filter_Zoo/Assets/Scripts/AmbientLight.cs
+++ Filter_Zoo/Assets/Scripts/AmbientLight.cs
@@ -4,21 +4,25 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 
-public class PortTest : MonoBehaviour
+public class AmbientLight : MonoBehaviour
 {
   // Start is called before the first frame update
+  public bool isConnected = false;
   SerialPort serialPort;
   Thread readSerialPortThread;
   public Light mainLight;
-  double brightness = 0;
+  double brightness = 1.0;
   // public Light mainLight;
   void Start()
   {
-    serialPort = new SerialPort(portName: "COM3", baudRate: 9600);
-    serialPort.Open();
-    serialPort.ReadTimeout = 50;
-    readSerialPortThread = new Thread(ReadSerialPort);
-    readSerialPortThread.Start();
+    if (isConnected)
+    {
+      serialPort = new SerialPort(portName: "COM3", baudRate: 9600);
+      serialPort.Open();
+      serialPort.ReadTimeout = 50;
+      readSerialPortThread = new Thread(ReadSerialPort);
+      readSerialPortThread.Start();
+    }
   }
 
   void ReadSerialPort()
@@ -32,7 +36,7 @@ public class PortTest : MonoBehaviour
         string line = serialPort.ReadLine();
         var measuredBrightness = int.Parse(line);
         queue.Enqueue(measuredBrightness);
-        if (queue.Count > 50)
+        if (queue.Count > 30)
         {
           queue.Dequeue();
         }
@@ -47,7 +51,6 @@ public class PortTest : MonoBehaviour
   void Update()
   {
     mainLight.intensity = (float)brightness;
-
   }
 
   void OnDestroy()

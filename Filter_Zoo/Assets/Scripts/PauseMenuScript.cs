00118ac037c3a4ad157b216320086f4601c3b724
Max Morgel <Max.Morgel@Student.HTW-Berlin.de>
2022-07-13 15:26:03 +0200

    added pause menu

---
+++ Filter_Zoo/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+   
+    public GameObject Menu;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Menu.activeSelf == false)
+            {
+                SetMenu();
+            }
+            else
+            {
+                CloseMenu();
+            }
+        }
+
+    }
+
+ public void SetMenu()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Menu.SetActive(true);
+        Time.timeScale = 0.0f;
+    }
+
+    public void CloseMenu()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Menu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Ich bin aus!");
+        Application.Quit();
+    }
+    
+}
